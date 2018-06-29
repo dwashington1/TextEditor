5917d3bfcefd4585552ab179d35d3cc00d117c1d
David Washington <washind9154@outlook.com>
2018-06-28 20:11:31 -0500

    Text Editor

---
+++ TextEditor/DataHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    public class DataHolder
+    {
+        public string Hold;
+        public DataHolder(string s)
+        {
+            Hold = s;
+        }
+    }
+}
---
+++ TextEditor/SaveBeforeClose.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class SaveBeforeClose : Form
+    {
+        public SaveBeforeClose()
+        {
+            InitializeComponent();
+        }
+
+        public bool clear = false;
+        public DataHolder contents;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clear = false;
+            Visible = false;
+            
+            Form1 temp = new Form1();
+            temp.contents = contents;
+            temp.Post_Click_NewCancelbutton();
+            temp.Visible = true;
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            Visible = false;
+            clear = true;
+            //Form1 temp = new Form1();
+            //temp.contents = contents;
+            //temp.Post_Click_NewCancelbutton();
+            //components = null;
+        }
+    }
+}
