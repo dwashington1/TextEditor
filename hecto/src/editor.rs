d2c5f024275d9e027f8cc53ba84b3cf096cc1129
dwashington1 <washind9154@gmail.com>
2021-04-15 19:28:41 -0500

    Added graceful exit

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -4,23 +4,28 @@ use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 #[derive(Default)]
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
             if let Err(error) = self.process_keypress() {
                 die(error);
             }
+            if self.should_quit {
+                break;
+            }
         }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
-        let key = read_key()?;
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let key = read_key()?;  // ? means if error, return that error right away
         match key {
-                Key::Ctrl('q') => panic!("Exiting..."),
+                Key::Ctrl('q') => self.should_quit = true,
                 _ => (),
         }
         Ok(())
